d928f612b29497194d842835306e45976acab13a
Matti Sirokov <matti@identio.fi>
2024-03-31 12:30:20 +0300

    Add shadcn to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 import AuthButton from "@/components/AuthButton";
 
+import { Button } from "@/components/ui/button";
+
 export default async function Index() {
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <div className="animate-in flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0">
+      <div className="animate-fadeInUp flex max-w-4xl flex-1 flex-col gap-20 px-3">
         <main className="flex flex-1 flex-col justify-center">
           <h2 className="mb-4 text-4xl font-bold">
             Slick - a better way to fix your car
           </h2>
           <div className="flex justify-center gap-4">
-            <Link href="/browse" className="bg-green p-4 text-white">
-              Browse repair shops
-            </Link>
+            <Button asChild variant="default" size="default">
+              <Link href="/browse">Browse Shops</Link>
+            </Button>
             <AuthButton />
           </div>
         </main>
