7751e71b5a12cafad2b05af31b0d8d5007bd0c5f
Matti Sirokov <matti@identio.fi>
2024-04-28 15:16:52 +0300

    remove reduntant component

--- app/(standard-pages)/browse/page.tsx
+++ app/(standard-pages)/browse/page.tsx
@@ -2,7 +2,10 @@ import { getShopsByFilter } from "@/server/repair-shops/actions";
 
 import BrowseMap from "@/components/browse-map/BrowseMap";
 
-import BrowsePageShopList from "@/components/browse-page/BrowsePageShopList";
+import BrowseSearch from "@/components/browse-page/BrowseSearch";
+import RepairShopCard from "@/components/cards/RepairShopCard";
+
+import { RepairShop } from "@/types";
 
 export default async function Browse({
   searchParams,
@@ -17,7 +20,25 @@ export default async function Browse({
 
   return (
     <main className="grid grid-cols-6 p-2">
-      <BrowsePageShopList shops={shops} />
+      <div className="relative col-span-3 min-h-screen bg-gray-100">
+        <BrowseSearch />
+        <div
+          className={
+            "flex max-h-screen flex-row flex-wrap overflow-auto pl-4 pr-4 pt-4"
+          }
+        >
+          {shops.map((shop: RepairShop) => (
+            <div className={"mb-4 w-1/2"} key={shop.id}>
+              <RepairShopCard
+                shopID={shop.id}
+                name={shop.name}
+                status={shop.status}
+                description={shop.description}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <div className={"relative col-span-3 h-full"}>
         <BrowseMap shops={shops} />
       </div>
